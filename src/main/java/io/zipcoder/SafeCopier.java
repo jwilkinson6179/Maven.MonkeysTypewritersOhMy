1f237517e98d9ab0c36a43530f20459587260dc4
jwilkinson6179 <JamesW6179@gmail.com>
2020-03-15 21:26:01 -0400

    ReentrantLock implementation

--- src/main/java/io/zipcoder/SafeCopier.java
+++ src/main/java/io/zipcoder/SafeCopier.java
@@ -1,25 +1,33 @@
 package io.zipcoder;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Make this extend the Copier like `UnsafeCopier`, except use locks to make sure that the actual intro gets printed
  * correctly every time.  Make the run method thread safe.
  */
 public class SafeCopier extends Copier
 {
+    ReentrantLock lock;
     public SafeCopier(String toCopy)
     {
         super(toCopy);
+        lock = new ReentrantLock();
     }
 
     public void run()
     {
-        synchronized (this)
+        lock.lock();
+        try
         {
             while (stringIterator.hasNext())
             {
                 String word = stringIterator.next();
                 copied += word + " ";
             }
+        } finally
+        {
+            lock.unlock();
         }
     }
 }
